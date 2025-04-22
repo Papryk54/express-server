ad9234a7f400e7c06b0d65bc3705f91183cc26e3
Patryk Olszewski <patrykolszewski420@gmail.com>
2025-04-22 17:41:18 +0200

    Extend contact page

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const express = require("express");
 const path = require("path");
 const hbs = require("express-handlebars");
+const multer = require("multer");
+const upload = multer();
 
 const app = express();
 app.engine("hbs", hbs());
 app.set("view engine", "hbs");
 
 app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use("/user", (req, res) => {
 	res.render("forbidden");
+});
+
+app.post("/contact/send-message", upload.single("design"), (req, res) => {
+	const { author, sender, title, message } = req.body;
+	const file = req.file;
+
+	if (author && sender && title && message && file) {
+		res.render("contact", {
+			isSent: true,
+			fileName: file.originalname,
+		});
+	} else {
+		res.render("contact", { isError: true });
+	}
 });
 
 app.get("/", (req, res) => {
@@ -20,8 +38,8 @@ app.get("/hello/:name", (req, res) => {
 	res.render("hello", { name: req.params.name });
 });
 
-app.get("/about", (req, res) => {
-	res.render("about.hbs", { layout: "dark" });
+app.get("/contact/send-message", (req, res) => {
+	res.render("contact.hbs");
 });
 
 app.get("/user/panel", (req, res) => {
